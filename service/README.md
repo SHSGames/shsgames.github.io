be581763864d203430d30c1d06c0821b42090e2c
Josh Merlino <joshua.merlino@gmail.com>
2019-10-25 19:49:07 -0400

    add ads to mirror again

---
+++ service/README.md
@@ -0,0 +1,3 @@
+Yeah, we know theres a private key here.
+`privkey.pem` is used to decrypt the userdata sent from the client to the server.
+Theres no secret or data that needs to be protected, also: it needs to be here to work so I dont feel like deleting this file.
--- service/web/fs/read.js
+++
@@ -1,9 +0,0 @@
-const request = require("request");
-
-module.exports = (req,res) => {
-	service("webservice")(req,res)((params, cookies) => {
-		request(`https://storage.googleapis.com/shsgames_storage/userdata/${cookies["x-storageapi"]}/${params.file}`, function(_,_,resp) {
-			res.end(resp)
-		});
-	});
-}
