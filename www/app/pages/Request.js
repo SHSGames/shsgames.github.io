956a461aae8425f2bd759b6f56277403208ed039
Josh Merlino <joshua.merlino@gmail.com>
2019-10-28 17:00:10 -0400

    update request forms

--- www/app/pages/Request.js
+++ www/app/pages/Request.js
@@ -4,21 +4,13 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 class Request extends React.Component {
-	constructor() {
-		super();
-
-		this._hrefs = ["https://docs.google.com/forms/d/e/1FAIpQLSe3iNjW7GjNa9gYzHQBbQrIYKrTrU4cojsLamjePURqkKHdtw/viewform?embedded=true"]
-		this.state = { i: 0 };
-	}
-
 	render() {
 		return (
 			<div>
 				<Navbar/>
 				<div className="container" style={{ maxWidth: 512 }}>
 					<div className="title"><h2>Request</h2></div>
-					{this.state.i + 1 !== this._hrefs.length && <p>Dont have access? Click <a onClick={() => this.setState({ i: this.state.i +1 })} className="primary-text" style={{ cursor: "pointer" }}>here</a>.</p>}
-					<iframe id="request-frame" src={this._hrefs[this.state.i]} width="500" height="868" frameBorder="0" marginHeight="0" marginWidth="0"></iframe>
+					<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdYfBSNxPLPYS87V8F5H_srzNHeMyR0GQ-ronruWZs1FzGbfQ/viewform?embedded=true" width="640" height="1512" frameBorder="0" marginHeight="0" marginWidth="0" style={{ margin: "0 -26px" }}>Loading…</iframe>
 				</div>
 				<Footer/>
 			</div>
