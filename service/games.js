a04116a90207e457cca5442af163dad2bdf35461
Josh Merlino <joshua.merlino@gmail.com>
2019-11-04 18:58:43 -0500

    Auto stash before merge of "master" and "origin/master"

--- service/games.js
+++ service/games.js
@@ -925,3 +925,762 @@ module.exports = {
         }]
     }]
 };
+
+		name: "Flash",
+		games: [{
+			name: "2048",
+			engine: "flash",
+			params: { aspectRatio: 480/572, width: 512, options: {} }
+		},
+		{
+			name: "4th and Goal 2018",
+			engine: "flash",
+			params: { aspectRatio: 12/9, options: {} }
+		},
+		{
+			name: "4x4 Soccer",
+			engine: "flash",
+			params: { width: 640, options: { "allowscriptaccess": "always" } }
+		},
+		{
+			name: "Angry Birds",
+			engine: "flash",
+			params: { options: { "allowscriptaccess": "always" } }
+		},
+		{
+			name: "B-Cubed",
+			engine: "flash",
+			params: { aspectRatio: 10/9, options: {} }
+		},
+		{
+			name: "Basketball Legends",
+			engine: "flash",
+			params: { aspectRatio: 5/3, options: {} }
+		},
+		{
+			name: "Bloons Tower Defense 5",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Bloons Tower Defense 4",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Bloons Tower Defense 3",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Bloons Tower Defense 2",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Bloons Tower Defense 1",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Bloxorz",
+			engine: "flash",
+			params: { aspectRatio: 16/9, options: {} }
+		},
+		{
+			name: "BMX Park",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Cat Ninja",
+			engine: "flash",
+			params: { width: 800, options: {} }
+		},
+		{
+			name: "Cubefield",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Dead Zed 2",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Doom Triple Pack",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Drag Racer v3",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Dragon Fist 3",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Duck Life 3",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Duck Life 2",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Duck Life 1",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Electric Man 2",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Epic Combo",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Escape the Bathroom",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Escape the Car",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Escape the Closet",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Escape the Freezer",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Feudalism 2",
+			engine: "flash",
+			params: { options: { "allowscriptaccess": "always" } }
+		},
+		{
+			name: "Flight",
+			engine: "flash",
+			params: { aspectRatio: 35/24, options: { } }
+		},
+		{
+			name: "Gun Mayhem 2",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Happy Wheels",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Henry Stickmin - Breaking the Bank",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Henry Stickmin - Crossing the Pit",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Henry Stickmin - Escaping the Prison",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Henry Stickmin - Fleeing the Complex",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Henry Stickmin - Infiltrating the Airship",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Henry Stickmin - Stealing the Diamond",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Hockey Legends",
+			engine: "flash",
+			params: { aspectRatio: 5/3, options: { "wmode": "direct", "menu": "false" } }
+		},
+		{
+			name: "Impossible Quiz",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Learn to Fly",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Learn to Fly 2",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Mutilate a Doll 2",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Ninja Painter",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Ninja Painter 2",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Papas Bakeria",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Pacman",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Papas Burgeria",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Papas Cheeseria",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Papas Cupcakeria",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Papas Donuteria",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Papas Freezeria",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Papas Hotdoggeria",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Papas Pancakeria",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Papas Pastaria",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Papas Pizzeria",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Papas Scooperia",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Papas Sushiria",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Papas Tacomia",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Papas Wingeria",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Parking Mania",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Pitch Hitter 2",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Pitch Hitter 3",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Poker",
+			engine: "flash",
+			params: { aspectRatio: 64/43, width: 640, options: {} }
+		},
+		{
+			name: "Potty Racers 3",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Raze 3",
+			engine: "flash",
+			params: { options: { "wmode": "window" } }
+		},
+		{
+			name: "Run 2",
+			engine: "flash",
+			params: { options: { "framerate": "60", "wmode": "direct", "data-target": "game.game" } }
+		},
+		{
+			name: "Run 3",
+			engine: "flash",
+			params: { options: { "framerate": "60", "wmode": "direct", "data-target": "game.game" } }
+		},
+		{
+			name: "Run n Gun",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Sports Head Basketball",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Sports Head Football",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Sports Head Ice Hockey",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Sports Head Ice Hockey Championship",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Sports Head Tennis",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Stickwar",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Street Fighter 2",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Strike Force Heroes",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Super Fighters",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Super Mario 63",
+			engine: "flash",
+			params: { aspectRatio: 800/(533 + 1/3), options: {} }
+		},
+		{
+			name: "Super Mario Crossover",
+			engine: "flash",
+			params: { aspectRatio: 510/478, width: 510, options: {} }
+		},
+		{
+			name: "Super Mario Flash",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Super Mario Flash 2",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Super Smash Flash",
+			engine: "flash",
+			params: { aspectRatio: 2/1, width: 640, options: {} }
+		},
+		{
+			name: "Swords and Sandals 1",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Swords and Sandals 2",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Swords and Sandals 3",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Tetris",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Theme Hotel",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Truck Loader 4",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Vex 1",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Vex 2",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Worlds Hardest Game",
+			engine: "flash",
+			params: { options: {} }
+		},
+		{
+			name: "Zombocalypse 2",
+			engine: "flash",
+			params: { options: { "framerate": "60", "wmode": "direct", "data-target":"game.game" } }
+		}]
+	}, {
+
+		name: "Gameboy Advance",
+		games: [{
+			name: "Advance Wars",
+			engine: "gba",
+			params: { options: {} }
+		}, {
+			name: "Fire Emblem",
+			engine: "gba",
+			params: { options: {} }
+		}, {
+			name: "Golden Sun",
+			engine: "gba",
+			params: { options: {} }
+		}, {
+			name: "Mario Kart",
+			engine: "gba",
+			params: { options: {} }
+		}, {
+			name: "Pacman World",
+			engine: "gba",
+			params: { options: {} }
+		}, {
+			name: "Pokemon Ash Gray",
+			engine: "gba",
+			params: { options: {} }
+		}, {
+			name: "Pokemon Dark Violet",
+			engine: "gba",
+			params: { options: {} }
+		}, {
+			name: "Pokemon Emerald",
+			engine: "gba",
+			params: { options: {} }
+		}, {
+			name: "Pokemon Flora Sky",
+			engine: "gba",
+			params: { options: {} }
+		}, {
+			name: "Pokemon Glazed",
+			engine: "gba",
+			params: { options: {} }
+		}, {
+			name: "Pokemon Green",
+			engine: "gba",
+			params: { options: {} }
+		}, {
+			name: "Pokemon Light Platinum",
+			engine: "gba",
+			params: { options: {} }
+		}, {
+			name: "Pokemon Ruby",
+			engine: "gba",
+			params: { options: {} }
+		}, {
+			name: "Pokemon Sapphire",
+			engine: "gba",
+			params: { options: {} }
+		}, {
+			name: "Pokemon Red",
+			engine: "gba",
+			params: { options: {} }
+		}, {
+			name: "Sonic Advance",
+			engine: "gba",
+			params: { options: {} }
+		}, {
+			name: "Wario Ware",
+			engine: "gba",
+			params: { options: {} }
+		}]
+	}, {
+
+		name: "NES",
+		games: [{
+			name: "Bubble Bobble",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "Blades of Steel",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "Castlevania",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "Contra",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "Double Dribble",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "Dr. Mario",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "Final Fantasy 1",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "Hoops",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "Ice Hockey",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "Kid Icarus",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "Kirby's Adventure",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "Legend of Zelda",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "Q-Bert",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "RC Pro-AM",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "Super Mario Bros",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "Super Mario Bros 3",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "Tecmo Bowl",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "Tecmo Super Bowl",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "Teenage Mutant Ninja Turtles",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "Totally Rad",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}, {
+			name: "Yoshi's Cookie",
+			engine: "nes",
+			params: {
+				aspectRatio: 16/15,
+				width: 640,
+				options: {}
+			}
+		}]
+
+	}, {
+
+		name: "SNES",
+		games: [{
+			name: "Super Mario World",
+			engine: "snes",
+			params: {
+				aspectRatio: 8/7,
+				width: 512,
+				options: {}
+			}
+		}]
+
+	}, {
+
+		name: "Unity",
+		games: [{
+			name: "Drift Hunters",
+			engine: "unity",
+			params: {
+				unityImage: "ItchIO",
+				aspectRatio: 16/9,
+				width: 1200,
+				options: {}
+			}
+		}, {
+			name: "Falling Ball",
+			engine: "unity",
+			params: {
+				unityImage: "slope-ball",
+				aspectRatio: 16/9,
+				width: 1200,
+				options: {}
+			}
+		}, {
+			name: "Slope",
+			engine: "unity",
+			params: {
+				unityImage: "slope_v7",
+				aspectRatio: 16/9,
+				width: 1200,
+				options: {}
+			}
+		}]
+
+	}]
+}
