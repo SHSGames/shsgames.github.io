1930146810e7dad344e14adf199d4698acacaaf9
Josh Merlino <joshua.merlino@gmail.com>
2019-11-15 11:59:05 -0500

    Ad upgrades

--- www/app/components/Adview.js
+++ www/app/components/Adview.js
@@ -7,22 +7,39 @@ class Adview extends React.Component {
 	}
 
 	componentDidMount() {
-		try {
-			(adsbygoogle = window.adsbygoogle || []).push({});
-		} catch(e) {
-			this.setState({ visible: false });
-			console.warn("Ads not shown. Network does not support.")
-		}
+		app.adsAvailibility().then(visible => this.setState({ visible }));
 	}
 
+	style = {
+		borderRadius: 4,
+		padding: 4
+	}
+
+	message = "We noticed you have some sort of ad blocker enabled. Ads like this are what keep this service free and running. We understand that some schools block ads and theres nothing you can do about it, in that case, tell all your friends from other schools to join."
+
 	render() {
-		return !this.state.visible ? null : (
-			<ins className="adsbygoogle"
-		      style={{ display: "block", marginTop: 8 }}
-		      data-ad-format="fluid"
-			  data-ad-layout-key="-g7-u+8l-8s-d3"
-     		  data-ad-client="ca-pub-6128732932572955"
-     		  data-ad-slot="8757072245"/>
+		setTimeout(Photon.reload)
+		return !this.state.visible ? (
+			<div style={{ background: "#52525210", ...this.style, ...this.props.style, height: this.props.height || "auto" }}>
+				<div style={{ display: "grid", height: "100%" }}>
+					<div style={{ margin: "auto" }}>
+						<center>
+							<div style={{ opacity: .54 }}>
+								<i className="material-icons" style={{ fontSize: 64, display: "inline-block", padding: "16px 32px" }}>sentiment_dissatisfied</i>
+								<div style={{ width: "80%", maxWidth: 600, textAlign: "left" }}>{this.message}</div>
+							</div>
+						</center>
+					</div>
+				</div>
+			</div>
+		) : (
+			<div style={{ background: "#52525210", ...this.style, ...this.props.style, height: this.props.height || "auto" }}>
+				<div style={{ display: "grid", height: "100%" }}>
+					<div style={{ margin: "auto" }}>
+						<ins className="adsbygoogle" {...this.props}/>
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
--- www/app/components/GameGroup.js
+++ www/app/components/GameGroup.js
@@ -9,7 +9,13 @@ class GameGroup extends React.Component {
 	render() {
 		return (
 			<div className="gamegroup">
-				<Adview/>
+				<Adview
+				  style={{ display: "block", marginTop: 8 }}
+				  height={200}
+			      data-ad-layout="in-article"
+			      data-ad-format="fluid"
+			      data-ad-client="ca-pub-6128732932572955"
+			      data-ad-slot="8241019118"/>
 				<div className="title">
 					<h2 id={app.slug(this.props.group.name)}>{this.props.group.name}</h2>
 				</div>
