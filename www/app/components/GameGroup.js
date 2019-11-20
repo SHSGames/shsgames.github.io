72ee0d27ed0c951159e68f3012c2abf9b1947f76
JoshMerlino <31785395+JoshMerlino@users.noreply.github.com>
2019-11-20 10:47:57 -0500

    Update GameGroup.js

--- www/app/components/GameGroup.js
+++ www/app/components/GameGroup.js
@@ -9,13 +9,15 @@ class GameGroup extends React.Component {
 	render() {
 		return (
 			<div className="gamegroup">
-				<Adview
+				<div style={{ marginTop: 8 }}>
+					<Adview
 				  style={{ display: "block", textAlign: "center" }}
 				  height={200}
 			      data-ad-layout="in-article"
 			      data-ad-format="fluid"
 			      data-ad-client="ca-pub-6128732932572955"
 			      data-ad-slot="8241019118"/>
+				</div>
 				<div className="title">
 					<h2 id={app.slug(this.props.group.name)}>{this.props.group.name}</h2>
 				</div>
