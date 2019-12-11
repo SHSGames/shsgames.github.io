c20fda0c204e03fc50b9dc704c2c0613eb28f529
Josh Merlino <joshua.merlino@gmail.com>
2019-12-10 20:25:05 -0500

    update sidenav

--- www/app/components/Sidenav.js
+++ www/app/components/Sidenav.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Link, Redirect } from "react-router-dom";
 
-import IMG_ICON_WEB_HI_RES_512_PNG from "../../img/icon/web_hi_res_512.png";
-
 class ExitLink extends React.Component {
 	constructor() {
 		super();
@@ -28,8 +26,7 @@ class Sidenav extends React.Component {
 	render() {
 		return (
 			<ul id="sn-webview" className="sidenav" style={{ margin: 0, padding: 0 }}>
-				<li><img src={IMG_ICON_WEB_HI_RES_512_PNG} alt="" style={{ padding: "1rem 25%" }} width="50%"/></li>
-				<li className="divider"></li>
+				<li><img src="/img/banner.png" alt=""/></li><br/>
 				<li><Link onClick={ () => $("#sn-webview").sidenav("close") } className="waves-effect" to="/"><i className="material-icons">home</i>Home</Link></li>
 				<li><Link onClick={ () => $("#sn-webview").sidenav("close") } className="waves-effect" to="/request"><i className="material-icons">library_add</i>Game Request</Link></li>
 				<li><a onClick={ () => $("#settings-view").addClass("active") } className="waves-effect"><i className="material-icons">settings</i>Settings</a></li>
