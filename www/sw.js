fc0edb647165da81c28482e7502f273d9d1b8cf4
Josh Merlino <joshua.merlino@gmail.com>
2020-01-22 11:28:01 -0500

    make serviceworker redirect document requests to index

--- www/sw.js
+++ www/sw.js
@@ -3,12 +3,14 @@ const CACHE = "application-cache";
 self.addEventListener("fetch", event => {
 	if(location.port !== "") return;
 	if(event.request.method === "GET") {
+		console.log(request.url, request.url.match(/\/\g\//g))
 		event.respondWith(fromCache(event.request));
 		event.waitUntil(update(event.request));
 	}
 });
 
 async function fromCache(request) {
+	// if(request.url.match(/\/\g\//g)) request.url = request.url.split("g/")[0];
     const cache = await caches.open(CACHE);
     const match = await cache.match(request);
     return match || fetch(request);
