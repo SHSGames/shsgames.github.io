21e66e300d31ec2a89da8babf13915e10a265955
Josh Merlino <joshua.merlino@gmail.com>
2020-03-10 17:35:15 -0400

    update fps counter

--- www/app/components/FrameCounter.js
+++ www/app/components/FrameCounter.js
@@ -7,31 +7,21 @@ class FrameCounter extends React.Component {
 		this._fps = 0;
 		this._mounted = false;
 		this._framecounter = performance.now();
-		this.state = { FPS: 0, refresh: 0 };
+		this.state = { max: 0, min: 0 };
+		let fps = [];
 
 		let _this = this;
 		(function frameCounter(){
 			requestAnimationFrame(frameCounter);
 			_this._fps = 1000/(performance.now() - _this._framecounter);
 			_this._framecounter = performance.now();
+			fps.push(_this._fps);
 		}());
-
-		const SAMPLE = 10;
-		const hz = [ 40, 60, 75, 90, 100, 120, 144, 165, 200, 240, 300 ];
-		let fps = [];
 
 		setInterval(() => {
 			if(this._mounted) {
-				let avgfps = (this._fps + fps);
-				fps.push(this._fps);
-
-				while(fps.length > SAMPLE) fps.shift();
-				avgfps = fps.reduce((a,b) => a + b, 0)/fps.length - 1;
-
-				this.setState({
-					FPS: Math.floor(this._fps),
-					refresh: hz.reduce((prev, curr) => Math.abs(curr - avgfps) < Math.abs(prev - avgfps) ? curr : prev)
-				});
+				this.setState({ max: Math.max(...fps), min: Math.min(...fps) });
+				fps = [];
 			}
 		}, 1000);
 	}
@@ -46,7 +36,7 @@ class FrameCounter extends React.Component {
 
 	render() {
 		return (
-			<div style={{ position: "absolute", top: "0px", left: "24px", lineHeight: "52px" }}>FPS: {this.state.FPS} - {screen.height}p@{this.state.refresh}hz</div>
+			<div style={{ position: "absolute", top: "0px", left: "24px", lineHeight: "52px" }}>FPS: {Math.trunc(this.state.max)}/{Math.trunc(this.state.min)}</div>
 		);
 	}
 }
