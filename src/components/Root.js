6bc81b10baec48e33196c3ef549fb28e641e97bc
Josh Merlino <31785395+JoshMerlino@users.noreply.github.com>
2020-04-27 22:40:38 -0400

    Update Root.js

--- src/components/Root.js
+++ src/components/Root.js
@@ -9,12 +9,37 @@ let ROUTES = [];
 const importAll = a => a.keys().forEach(k => ROUTES.push(a(k).default));
 importAll(require.context("../views", true, /\.js$/));
 
-const Root = () => (
-	<Router>
-		<main>
-			{ ROUTES.map(({ route, View }, key) => <Route key={key} path={route} exact={true} component={View}/> ) }
-		</main>
-	</Router>
-);
+// Create root component
+class Root extends React.Component {
+
+	// Moniter for route changes to reload Photon
+	componentDidMount() {
+		let route = app.getRoute();
+		(function loop() {
+			requestAnimationFrame(loop);
+			if(route !== app.getRoute()) {
+				route = app.getRoute();
+				$(window).scrollTop(0);
+				Photon.reload();
+			}
+		}());
+	}
+
+    render() {
+
+		// Initial render of root component
+		return (
+			<Router>
+				<Sidenav/>
+				<main>
+					{ ROUTES.map(({ route, View }, key) => <Route key={key} path={route} exact={true} component={View}/> ) }
+				</main>
+				<Footer/>
+			</Router>
+        );
+
+    }
+
+}
 
 export default Root;
