6d3e63c35c40c734959a803b87115bae625bb95d
Josh Merlino <joshua.merlino@gmail.com>
2020-07-23 14:27:54 -0400

    exclude compilation of modules

--- webpack.conf.js
+++ webpack.conf.js
@@ -19,7 +19,7 @@ module.exports = {
         rules: [
 			{
 	            test: /\.js$/,
-	            //exclude: /(node_modules)/,
+	            exclude: /(node_modules)/,
 	            use: {
 	                loader: "babel-loader",
 	                options: {
