e3ef9034d46f99817fb9062f6fc967bb9cd0cdda
Josh Merlino <joshua.merlino@gmail.com>
2021-06-15 13:17:16 -0400

    mini change

--- web/ts/views/Index.tsx
+++ web/ts/views/Index.tsx
@@ -3,10 +3,9 @@ import React from "react";
 export const route = "/";
 
 export default function View(): JSX.Element {
-
-	console.log({ APP_MANIFEST });
-
-	return <>
-		<p>get started by editing `~/my-app/src/ts/views/Index.tsx::View`</p>
-	</>;
+	return (
+		<>
+			<p>get started by editing `~/my-app/src/ts/views/Index.tsx::View`</p>
+		</>
+	);
 }
