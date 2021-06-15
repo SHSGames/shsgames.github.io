3fafee4e3d5c886a86921a58fccd5a3d0d4deeec
Josh Merlino <31785395+JoshMerlino@users.noreply.github.com>
2021-06-15 13:15:53 -0400

    Update gzip.ts

--- src/middleware/gzip.ts
+++ src/middleware/gzip.ts
@@ -2,6 +2,6 @@ import compression from "compression";
 
 // Export middleware
 export default compression({
-	filter: (req) => req.query.hasOwnProperty("decompressed") || req.header("use-gzip") === "false",
+	filter: (req) => !req.query.hasOwnProperty("decompressed") && req.header("use-gzip") !== "false",
 	level: 8
 });
