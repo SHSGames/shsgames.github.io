0aa99df6e0bf89334d43a6c22a59ffbfd9160f62
Josh Merlino <joshua.merlino@gmail.com>
2021-09-01 22:20:46 -0400

    start game renderer

--- web/ts/components/GameManager.tsx
+++ web/ts/components/GameManager.tsx
@@ -5,6 +5,14 @@ export default function GameManager(): JSX.Element {
 	return (
 		<div className="game-manager">
 			<p className="frame-counter mono">FPS: <FrameStats/></p>
+			<p className="link mono" style={{
+				float: "right",
+				color: "var(--palette_primary_light)",
+				fontWeight: 500,
+				cursor: "pointer",
+				userSelect: "none"
+			}}
+			onClick={ () => $(".game-renderer")[0].requestFullscreen() }>Fullscreen</p>
 		</div>
 	);
 }
--- web/ts/components/GameRenderer.tsx
+++ web/ts/components/GameRenderer.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import { Spinner, VHCenter } from "photoncss/lib/react";
+import React, { useEffect, useState } from "react";
 import { Game } from "../../games";
 
 export type Props = { game: Game };
 export default function GameRenderer({ game }: Props): JSX.Element {
-	return (
-		<>
 
-		</>
+	const [ height, setHeight ] = useState(0);
+
+	useEffect(updateHeight, []);
+	$(window).on("resize", updateHeight);
+
+	function updateHeight() {
+		const { aspectRatio = 4/3 } = game;
+		setHeight(1 / aspectRatio * $(".game-renderer")[0].clientWidth);
+	}
+
+	return (
+		<div className="game-renderer" style={{ height }}>
+			<VHCenter>
+				<Spinner/>
+			</VHCenter>
+		</div>
 	);
 }
