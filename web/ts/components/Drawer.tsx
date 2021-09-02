06f1852ddec74984288a8261029172e65ae1e99a
Josh Merlino <joshua.merlino@gmail.com>
2021-09-02 19:00:48 -0400

    add drawer

--- web/ts/components/Drawer.tsx
+++ web/ts/components/Drawer.tsx
@@ -1,10 +1,21 @@
-import { Drawer } from "photoncss/lib/react";
+import { Drawer, ListSubheader } from "photoncss/lib/react";
 import React from "react";
+import { getGameID, slug } from "../src/gameHash";
+import games, { Game } from "../../games";
+import app from "../src/app";
+import { DrawerEntry } from "./DrawerEntry";
 
 export default function Component(): JSX.Element {
 	return (
 		<Drawer id="drawer">
-			lol
+			<img src={ app.static("icon.png") } alt="" style={{ width: "50%", display: "block", margin: "2rem auto" }}/>
+			<hr/>
+			<DrawerEntry to="/" icon="home">Home</DrawerEntry>
+			<hr/>
+			<ListSubheader>Games</ListSubheader>
+			{ games
+				.sort((a: Game, b: Game) => a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1)
+				.map((game: Game, key) => <DrawerEntry key={key} to={`/g/${getGameID(game)}/${slug(game.name)}`} icon="sports_esports">{game.name}</DrawerEntry>)}
 		</Drawer>
 	);
 }
