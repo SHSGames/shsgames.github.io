3ed6dd479be253029c4ac1d6845f870a1ba3a438
Josh Merlino <joshua.merlino@gmail.com>
2021-09-02 23:41:33 -0400

    add badge to drawer

--- web/ts/components/Drawer.tsx
+++ web/ts/components/Drawer.tsx
@@ -12,7 +12,7 @@ export default function Component(): JSX.Element {
 			<hr/>
 			<DrawerEntry to="/" icon="home">Home</DrawerEntry>
 			<hr/>
-			<ListSubheader>Games</ListSubheader>
+			<ListSubheader>Games <span className="badge" style={{ marginLeft: 16, fontSize: 12, padding: "0px 8px" }}>{ games.length } Games</span></ListSubheader>
 			{ games
 				.sort((a: Game, b: Game) => a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1)
 				.map((game: Game, key) => <DrawerEntry key={key} to={`/g/${getGameID(game)}/${slug(game.name)}`} icon="sports_esports">{game.name}</DrawerEntry>)}
