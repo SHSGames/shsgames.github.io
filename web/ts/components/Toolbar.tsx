521aca33bd0146930f66218712c30b14ad5b18bb
Josh Merlino <joshua.merlino@gmail.com>
2021-09-02 22:55:05 -0400

    add link in toolbar

--- web/ts/components/Toolbar.tsx
+++ web/ts/components/Toolbar.tsx
@@ -1,6 +1,7 @@
 import Photon from "photoncss";
 import { Icon, Toolbar, ToolbarActions, ToolbarTitle } from "photoncss/lib/react";
 import React from "react";
+import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import { SearchToggleButton } from "./SearchToggleButton";
 import ThemeSwitcher from "./ThemeSwitcher";
@@ -9,7 +10,11 @@ export default function Component(): JSX.Element {
 	return (
 		<Toolbar>
 			<Icon onClick={ () => Photon.Drawer("#drawer").open() }>menu</Icon>
-			<ToolbarTitle>{ APP_MANIFEST.name }</ToolbarTitle>
+			<ToolbarTitle>
+				<Link to="/">
+					{ APP_MANIFEST.name }
+				</Link>
+			</ToolbarTitle>
 			<SearchBar/>
 			<ToolbarActions>
 				<SearchToggleButton/>
