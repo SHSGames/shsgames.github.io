2c56686d700e1258ce0044672d1ba6a67d5042c2
Josh Merlino <joshua.merlino@gmail.com>
2021-09-03 11:21:46 -0400

    fix #37

--- web/ts/components/Adsense.tsx
+++ web/ts/components/Adsense.tsx
@@ -1,6 +1,5 @@
 import { Card } from "photoncss/lib/react";
-import React, { CSSProperties } from "react";
-import GoogleAdsense from "react-adsense-google";
+import React, { CSSProperties, useEffect } from "react";
 
 export type Props = {
 	adLayout: "in-article";
@@ -11,14 +10,14 @@ export type Props = {
 export default function Adsense({ adLayout = "in-article", style, adFormat = "fluid", fullWidthResponsive = "true" }: Props): JSX.Element {
 	return (
 		<Card variant="outlined" style={{ overflow: "hidden" }}>
-			<GoogleAdsense
-				adClient="ca-pub-6128732932572955"
-				adSlot="1159034500"
+			<ins className="adsbygoogle"
 				style={style}
-				adLayout={adLayout}
-				adFormat={adFormat}
-				fullWidthResponsive={fullWidthResponsive}
-			/>
+				data-ad-client="ca-pub-6128732932572955"
+				data-ad-slot="1159034500"
+				data-ad-layout={adLayout}
+				data-ad-format={adFormat}
+				data-full-width-responsive={fullWidthResponsive}></ins>
+			<script>{"(adsbygoogle = window.adsbygoogle || []).push({});"}</script>
 		</Card>
 	);
 }
