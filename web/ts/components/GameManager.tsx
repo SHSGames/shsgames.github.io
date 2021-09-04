45a2f172251847cde7bc265216a04ee6e7f1b88d
Josh Merlino <joshua.merlino@gmail.com>
2021-09-04 14:14:23 -0400

    add f hotkey to fullscreen

--- web/ts/components/GameManager.tsx
+++ web/ts/components/GameManager.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import FrameStats from "./FrameStats";
 
 export default function GameManager(): JSX.Element {
+	useEffect(function() {
+		$(document).on("keypress", function (event) {
+			if (event.shiftKey) return;
+			const search = $(".search")
+				.children(".photon-input")
+				.children("input");
+			if (search.is(":focus")) return;
+			if (event.key !== "f") return;
+			$("#game-renderer")[0].requestFullscreen();
+		});
+	});
+
 	return (
 		<div className="game-manager">
 			<p className="frame-counter mono">FPS: <FrameStats/></p>
