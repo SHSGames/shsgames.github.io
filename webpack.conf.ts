f1c8954bfe80a446b94313dbd0b794ee41f110f6
Josh Merlino <31785395+JoshMerlino@users.noreply.github.com>
2021-10-04 18:55:48 -0400

    Update webpack.conf.ts

--- webpack.conf.ts
+++ webpack.conf.ts
@@ -105,7 +105,7 @@ export = <Configuration>{
 		}),
 		new DefinePlugin({
 			APP_MANIFEST: JSON.stringify(manifest),
-			APP_CONFIG: JSON.stringify(YAML.parse(fs.readFileSync(path.resolve("config.yml"), "utf8") || ""))
+			APP_CONFIG: JSON.stringify(YAML.parse(fs.existsSync(path.resolve("config.yml")) ? fs.readFileSync(path.resolve("config.yml"), "utf8") : ""))
 		}),
 		new OfflinePlugin
 	],
