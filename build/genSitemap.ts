977fa9cda54fb16ad2a213c9a64bf36b7410467c
Josh Merlino <joshua.merlino@gmail.com>
2021-12-07 14:44:38 -0500

    emit all HTML files to their proper locations so adsense dosnt have a fit

--- build/genSitemap.ts
+++ build/genSitemap.ts
@@ -1,4 +1,5 @@
-import { readFile, writeFile } from "fs/promises";
+import { copyFile, readFile, writeFile } from "fs/promises";
+import mkdirp from "mkdirp";
 import path from "path";
 import games from "../web/games";
 import { getGameID, slug } from "../web/ts/src/gameHash";
@@ -7,9 +8,15 @@ const template = path.resolve("../../web/sitemap.txt");
 (async function(){
 
 	let sitemap = await readFile(template, "utf8");
-	games.map(game => {
-		sitemap += `\n/g/${getGameID(game)}/${slug(game.name)}`;
-	});
+	await Promise.all(games
+		.sort((a, b) => (a.name > b.name) ? 1 : -1)
+		.map(async game => {
+			sitemap += `\n/g/${getGameID(game)}/${slug(game.name)}`;
+			const out = path.resolve(`../../public_html/g/${getGameID(game)}/${slug(game.name)}.html`)
+			await mkdirp(path.resolve(out, ".."));
+			await copyFile(path.resolve("../../public_html/index.html"), out);
+		})
+	);
 
 	await writeFile(path.resolve("../../public_html/sitemap.txt"), sitemap + "\n", "utf8");
 
