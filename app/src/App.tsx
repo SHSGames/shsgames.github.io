255955d3205b274bef97e6392db94806214074df
Josh Merlino <joshua.merlino@gmail.com>
2022-03-06 16:31:26 -0500

    clean up

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -11,29 +11,21 @@ export default function App() {
 				<img src={ logo } className="App-logo" alt="logo" />
 				<p>Hello Vite + React!</p>
 				<p>
-					<button type="button" onClick={ () => setCount((count) => count + 1) }>
-						count is: { count } bruh
-					</button>
+					<button type="button" onClick={ () => setCount((count) => count + 1) }>count is: { count }</button>
 				</p>
-				<p>
-					Edit <code>App.tsx</code> and save to test HMR updates.
-				</p>
+				<p>Edit <code>App.tsx</code> and save to test HMR updates.</p>
 				<p>
 					<a
 						className="App-link"
 						href="https://reactjs.org"
 						target="_blank"
-						rel="noopener noreferrer">
-						Learn React
-					</a>
+						rel="noopener noreferrer">Learn React</a>
 					{ " | " }
 					<a
 						className="App-link"
 						href="https://vitejs.dev/guide/features.html"
 						target="_blank"
-						rel="noopener noreferrer">
-						Vite Docs
-					</a>
+						rel="noopener noreferrer">Vite Docs</a>
 				</p>
 			</header>
 		</div>
