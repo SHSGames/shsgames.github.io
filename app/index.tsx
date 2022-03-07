8800e529ad06e3aaf831a2dde0e61a214e068e4d
Josh Merlino <joshua.merlino@gmail.com>
2022-03-07 10:52:30 -0500

    sw mods

--- app/index.tsx
+++ app/index.tsx
@@ -4,11 +4,8 @@ import "./styles/index.css";
 import App from "./src/App";
 
 import { registerSW } from "virtual:pwa-register";
-if ("serviceWorker" in navigator/* && !/localhost/.test(window.location.toString()) */) registerSW({
-	immediate: true,
-	onRegistered(registration) {
-		console.log("SW registered: ", registration);
-	}
+if ("serviceWorker" in navigator && !/localhost/.test(window.location.toString())) registerSW({
+	immediate: true
 });
 
 ReactDOM.render(
