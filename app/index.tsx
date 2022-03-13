72249f1c44d88d4fe586fbe383d605dcb156e229
Josh Merlino <joshua.merlino@gmail.com>
2022-03-13 10:44:27 -0400

    setup react query

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,7 @@
 import { ElementType, StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { registerSW } from "virtual:pwa-register";
 import ErrorBoundary from "./src/runtime/ErrorBoundry";
@@ -9,22 +11,27 @@ if ("serviceWorker" in navigator && !/localhost/.test(window.location.toString()
 	immediate: true
 });
 
+export const queryClient = new QueryClient;
+
 export type Page = { default: ElementType, path: string, caseSensitive?: boolean };
 const pages = import.meta.globEager<Page>("./src/pages/*.tsx");
 
 ReactDOM.render(
 	<StrictMode>
 		<ErrorBoundary>
-			<BrowserRouter>
-				<Routes>
-					{ Object.values(pages).map((page, key) => <Route
-						key={ key }
-						path={ page.path }
-						caseSensitive={ page.caseSensitive || false }
-						element={ <page.default/> }/>
-					) }
-				</Routes>
-			</BrowserRouter>
+			<QueryClientProvider client={ queryClient }>
+				<BrowserRouter>
+					<Routes>
+						{ Object.values(pages).map((page, key) => <Route
+							key={ key }
+							path={ page.path }
+							caseSensitive={ page.caseSensitive || false }
+							element={ <page.default/> }/>
+						) }
+					</Routes>
+				</BrowserRouter>
+				{ !PRODUCTION && <ReactQueryDevtools/> }
+			</QueryClientProvider>
 		</ErrorBoundary>
 	</StrictMode>,
 	document.getElementById("root")
