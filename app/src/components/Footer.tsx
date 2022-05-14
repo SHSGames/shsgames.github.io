33d3a79409b8da76b9845c5e55a358722200d422
Josh Merlino <joshua.merlino@gmail.com>
2022-05-14 12:50:20 -0400

    add version to footer

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -15,9 +15,12 @@ export default function Footer(): JSX.Element {
 					<DiscordInvite guild="584437072618717209"/>
 				</div>
 				<div className="w-full text-gray-300 font-medium bg-black/10 h-12 flex justify-center items-center font-manrope">
-					<div className="mx-auto flex max-w-full md:max-w-[80%] w-[1280px] px-12 z-[4] flex-col text-sm">
+					<div className="mx-auto flex max-w-full md:max-w-[80%] w-[1280px] px-12 z-[4] flex-col text-sm relative">
 						<span>Copyright © 2019 - 2022 • All Rights Reserved</span>
 						<a href="//joshmerlino.github.io"><span className="text-primary">Josh Merlino <span className="text-xs font-condensed">LLC</span></span></a>
+						<div className="absolute right-0 top-0 bottom-0 flex items-center">
+							Version: <code className="border-[1px] border-neutral-500/20 rounded-md px-1 ml-2">v18.13.2</code>
+						</div>
 					</div>
 				</div>
 			</div>
