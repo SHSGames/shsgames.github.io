84517fe90c2c14b71f267825522444f41c6a67ba
Josh Merlino <joshua.merlino@gmail.com>
2022-05-27 12:18:15 -0400

    show game details in game runner window

--- app/src/components/GameRunner.tsx
+++ app/src/components/GameRunner.tsx
@@ -1,6 +1,16 @@
+import qs from "qs";
+import ghash from "../util/hash";
+
 export default function GameRunner(): JSX.Element {
-	console.log(location.search);
+	const { game, hash } = qs.parse(location.search.substring(1));
+	const matches = ghash(atob(game!.toString()), 36) === hash;
 	return (
-		<span>{location.search}</span>
+		<pre>
+			<code>
+				{JSON.stringify(JSON.parse(atob(game!.toString())), null, 4)}
+			</code>
+			<p>hash: {hash?.toString()}</p>
+			<p>ok: {matches.toString()}</p>
+		</pre>
 	);
 }
