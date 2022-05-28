ed3d3ed295dce3154e62a91cd8a34d4a84ef3d9a
Josh Merlino <joshua.merlino@gmail.com>
2022-05-28 13:41:36 -0400

    clean up

--- app/src/components/Adsense.tsx
+++ app/src/components/Adsense.tsx
@@ -1,24 +1,22 @@
-import { CSSProperties, useEffect } from "react";
+import { CSSProperties, useEffect, useState } from "react";
 
 export default function Adsense({ style }: { style?: CSSProperties }): JSX.Element {
 
-	useEffect(function() {
-		try {
-			// eslint-disable-next-line no-extra-parens
-			((window as any).adsbygoogle = (window as any).adsbygoogle || []).push({});
-		} catch (e) {
-			console.error(e);
-		}
+	const [ enabled ] = useState(false);
 
-	}, []);
-
-	return (
+	if (enabled) return (
 		<div className="flex md:min-w-[360px] max-w-[calc(100vw_-_32px)] min-h-[360px] grow" style={style}>
 			<ins className="adsbygoogle"
 				style={{ display: "block" }}
 				data-ad-format="autorelaxed"
 				data-ad-client="ca-pub-6128732932572955"
 				data-ad-slot="5379179914"></ins>
+		</div>
+	);
+
+	return (
+		<div className="flex md:min-w-[360px] max-w-[calc(100vw_-_32px)] min-h-[360px] grow bg-yellow-500/10">
+
 		</div>
 	);
 }
