701c36fdc0b01755b97cc5eedc2f0ff6e0394602
Josh Merlino <joshua.merlino@gmail.com>
2022-07-01 17:50:09 -0400

    use hash in .env

--- app/src/util/hash.ts
+++ app/src/util/hash.ts
@@ -1,6 +1,6 @@
 import { v5 as uuid } from "uuid";
 
 export default function hash(name: string, length = 8): string {
-	return uuid(name, "abe8da62-0c66-11ec-9f18-8be62656cabb")
+	return uuid(name, ENV.HASH)
 		.substring(0, length);
 }
