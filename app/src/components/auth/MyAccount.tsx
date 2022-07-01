9a33644b059c8ef23a13f0da930575d081cdec49
Josh Merlino <joshua.merlino@gmail.com>
2022-06-30 22:45:46 -0400

    add inputs to update userdata in the future

--- app/src/components/auth/MyAccount.tsx
+++ app/src/components/auth/MyAccount.tsx
@@ -1,13 +1,15 @@
 import classnames from "classnames";
-import { useState } from "react";
+import React, { useContext, useState } from "react";
 import { IoCloseCircleOutline } from "react-icons/io5";
 import { User } from "../../hooks/useUser";
+import { GunContext } from "../../runtime/GunContext";
 
 export let setShown: React.Dispatch<React.SetStateAction<boolean>>;
 
-export type Props = { user: false | User | null }
-export default function MyAccount({ user }: Props): JSX.Element {
+export type Props = { user: User }
+export default function MyAccount({ user }: Props): JSX.Element | null {
 
+	const db = useContext(GunContext);
 	const [ isShown, __setShown ] = useState(false);
 	setShown = __setShown;
 
@@ -17,7 +19,19 @@ export default function MyAccount({ user }: Props): JSX.Element {
 				<h1 className="text-4xl font-unisans whitespace-nowrap text-center flex flex-col relative">My Account
 					<IoCloseCircleOutline className="bg-black dark:bg-white !bg-opacity-5 hover:!bg-opacity-10 active:!bg-opacity-20 rounded-full p-1 text-black dark:text-white text-opacity-50 self-end absolute top-0 right-0 m-[2px]" onClick={ () => setShown(false) }/>
 				</h1>
-				<hr className="dark:border-zinc-600 -mx-4 my-4"/>
+				<hr className="dark:border-zinc-600 -mx-4 mt-4"/>
+
+				<div className="h-14 flex items-center">
+					<span>Username:</span>
+					<input className="bg-transparent grow ml-4 autofill:!bg-amber-500/10 my-2 ring-1 ring-zinc-500/50 outline-none focus:ring-2 focus:ring-primary py-1.5 px-3 rounded-xl" placeholder={user.username} defaultValue={user.username} type="text" name="username"/>
+				</div>
+				<hr className="dark:border-zinc-600 -mx-4"/>
+				<div className="h-14 flex items-center">
+					<span>Email Address:</span>
+					<input className="bg-transparent grow ml-4 autofill:!bg-amber-500/10 my-2 ring-1 ring-zinc-500/50 outline-none focus:ring-2 focus:ring-primary py-1.5 px-3 rounded-xl invalid:!ring-red-500" placeholder={user.email} defaultValue={user.email} type="email" name="email"/>
+				</div>
+
+				<hr className="dark:border-zinc-600 -mx-4 mb-4"/>
 				<pre className="overflow-hidden">
 					<code>
 						{JSON.stringify(user, null, 2)}
