e6445e6c7606522128a3a4c850f6cc1c2222e6b6
Josh Merlino <joshua.merlino@gmail.com>
2022-06-30 20:43:15 -0400

    link peers

--- app/src/runtime/GunContext.tsx
+++ app/src/runtime/GunContext.tsx
@@ -1,9 +1,10 @@
-import GUN, { IGunInstance } from "gun";
+import Gun, { IGunInstance } from "gun";
 import "gun/sea";
 import { createContext } from "react";
 
-export const gun = new GUN([
-	"https://gun.joshmerlino.me/gun"
+export const gun = new Gun([
+	"http://joshmerlino.me:3333/gun",
+	"https://gun-manhattan.herokuapp.com/gun"
 ]);
 
 export const GunContext = createContext<IGunInstance<any>>(gun);
