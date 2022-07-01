b521c804a4c5dc2b4c19659d32b2214504490a7f
Josh Merlino <joshua.merlino@gmail.com>
2022-07-01 13:56:58 -0400

    disable usermodification

--- app/src/components/auth/MyAccount.tsx
+++ app/src/components/auth/MyAccount.tsx
@@ -17,9 +17,9 @@ export default function MyAccount({ user }: Props): JSX.Element | null {
 				<h1 className="text-4xl font-unisans whitespace-nowrap text-center flex flex-col relative">My Account
 					<IoCloseCircleOutline className="bg-black dark:bg-white !bg-opacity-5 hover:!bg-opacity-10 active:!bg-opacity-20 rounded-full p-1 text-black dark:text-white text-opacity-50 self-end absolute top-0 right-0 m-[2px]" onClick={ () => setShown(false) }/>
 				</h1>
-				<hr className="dark:border-zinc-600 -mx-4 mt-4"/>
+				<hr className="dark:border-zinc-600 -mx-4 my-4"/>
 
-				<div className="h-14 flex items-center">
+				{/* <div className="h-14 flex items-center">
 					<span>Username:</span>
 					<input className="bg-transparent grow ml-4 autofill:!bg-amber-500/10 my-2 ring-1 ring-zinc-500/50 outline-none focus:ring-2 focus:ring-primary py-1.5 px-3 rounded-xl" placeholder={user.username} defaultValue={user.username} type="text" name="username"/>
 				</div>
@@ -27,9 +27,9 @@ export default function MyAccount({ user }: Props): JSX.Element | null {
 				<div className="h-14 flex items-center">
 					<span>Email Address:</span>
 					<input className="bg-transparent grow ml-4 autofill:!bg-amber-500/10 my-2 ring-1 ring-zinc-500/50 outline-none focus:ring-2 focus:ring-primary py-1.5 px-3 rounded-xl invalid:!ring-red-500" placeholder={user.email} defaultValue={user.email} type="email" name="email"/>
-				</div>
+				</div> */}
 
-				<hr className="dark:border-zinc-600 -mx-4 mb-4"/>
+				{/* <hr className="dark:border-zinc-600 -mx-4 mb-4"/> */}
 				<pre className="overflow-hidden">
 					<code>
 						{JSON.stringify(user, null, 2)}
